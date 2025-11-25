e247a41f788be6276d6c25f7548dc04fe6ae5f88
VishSeran <vishwa.sovis@gmail.com>
2025-11-25 20:59:21 +0530

    updated user register controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import User from "../models/userModel";
 
 
 //register client user
@@ -11,7 +12,25 @@ export const register = async (req: Request, res: Response) => {
             return res.status(400).json({ message: "Require all fields" });
         }
 
-        const existing = await User
+        const existing = await User.findOne({email});
+        if(existing){
+            return res.status(400).json({message: "email already registered"});
+        }
+
+        console.log(req.body);
+
+        const user = await User.create({
+            name,
+            email,
+            password,
+            role: role || "user",
+            kycStatus: "pending"
+        });
+
+        console.log("User created",user);
+        return res.status(500).json({message:"User created successfully"});
+
+        
     }catch(error){
 
     }
